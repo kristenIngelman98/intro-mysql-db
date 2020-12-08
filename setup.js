c03b77e50c1b9e643b0f156ffb35014862492245
Kristen Ingelman <kristeningelman@hotmail.com>
2020-12-08 09:29:19 -0800

    added connectionPool query to add table animals to database hello_mysql

--- setup.js
+++ setup.js
@@ -1,5 +1,22 @@
 const {connectionPool} = require('./connection.js');
 
-connectionPool.query(``, (error, results)=>{
+connectionPool.query(`
+    CREATE TABLE animals (
+      name  varchar(255)  NOT NULL,
+      type  varchar(255)  DEFAULT "dog"   NOT NULL,
+      id    int           AUTO_INCREMENT,
+      PRIMARY KEY(id) 
+    );
+
+`, (error, results)=>{
+      if(error) {
+        console.log(error);
+        process.exit();
+      }
+      else {
+        console.log("Success!");
+        console.log(results);
+        process.exit();
+    }
 
 });
