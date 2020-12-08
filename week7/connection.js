b0909c758664baf0ed0cd688cb9cf6fe6ed25814
Kristen Ingelman <kristeningelman@hotmail.com>
2020-12-08 11:23:20 -0800

    installed dotenv dependency

--- week7/connection.js
+++ week7/connection.js
@@ -1,11 +1,13 @@
-const promise_mysql = require("primise-mysql");
+require("dotenv").config();
+
+const promise_mysql = require("promise-mysql");
 
 const connectionPool = promise_mysql.createPool({
     multipleStatements: true,
-    host: "",
-    user: "",
-    password: "",
-    database: ""
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 
