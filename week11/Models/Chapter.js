6cdbdef828f19dd4cf04b56abc1d9128240e9442
Kristen Ingelman <kristeningelman@hotmail.com>
2020-12-09 17:51:26 -0800

    created Chapter Schema and compiled it into a Model Chapter

---
+++ week11/Models/Chapter.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+// constructor
+const Schema = mongoose.Schema;
+
+const chapterSchema = new Schema({
+  // put properties you would like each book to have
+  title: {type: String, required: true, maxLength:100},
+  text: String,
+  pages: {type:Number, min:1}
+
+});
+
+// compile into a Model
+exports.Chapter = mongoose.model('Book', chapterSchema);
