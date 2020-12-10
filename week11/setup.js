11df9d2b80a047a1d2cb2cbbb738600934276e2e
Kristen Ingelman <kristeningelman@hotmail.com>
2020-12-09 21:28:53 -0800

    finished adding book2

--- week11/setup.js
+++ week11/setup.js
@@ -7,49 +7,61 @@ const db = require("./connection.js");
 db.once("open", ()=>{
   console.log("connected!");
 
-  // creating a new book document by calling the book model we created earlier
-  // using a constructor function
-  // param constructor will take is an object
-  const book1 = new Book({
-    title: "Gardening for beginners",
-    author: "Kristen Ingelman"
-  });
-
-  book1.save(error=>{
-    // any code that relies on this document being saved should go here
+  // we want to delete all books in the db
+  Book.deleteMany({}, error=>{
     if(error) {
       console.log(error);
       process.exit();
     }
     else {
-      // if successful, create a new chapter
-      const chapter1 = new Chapter({
-        title: "Prologue",
-        text: "Gardening is easy",
-        pages:3
+      const book1 = new Book({
+        title: "Gardening for beginners",
+        author: "Kristen Ingelman"
       });
-
-      const chapter2 = new Chapter({
-        title: "Chapter2",
-        text: "Gardening is fun",
-        pages:5
-      });
-
-      book1.chapters.push(chapter1);
-      book1.chapters.push(chapter2);
-
+    
       book1.save(error=>{
+        // any code that relies on this document being saved should go here
         if(error) {
           console.log(error);
           process.exit();
-
-        } else {
-          console.log(book1, `This book has ${book1.pages} pages`);
-
         }
-      });
+        else {
+          // if successful, create a new chapter
+          const chapter1 = new Chapter({
+            title: "Prologue",
+            text: "Gardening is easy",
+            pages:3
+          });
+    
+          const chapter2 = new Chapter({
+            title: "Chapter2",
+            text: "Gardening is fun",
+            pages:5
+          });
+    
+          book1.chapters.push(chapter1);
+          book1.chapters.push(chapter2);
+    
+          book1.save(error=>{
+            if(error) {
+              console.log(error);
+              process.exit();
+    
+            } else {
+              console.log(book1, `This book has ${book1.pages} pages`);
+    
+            }
+          });
+        }
+      }); // save and store document to the db (asynchronous!) - END OF BOOK1
+
     }
-  }); // save and store document to the db (asynchronous!)
+  });  // end of delete many
+
+  // creating a new book document by calling the book model we created earlier
+  // using a constructor function
+  // param constructor will take is an object
+  
 
 
   
