3252c1d00a11bcdac49526fa752f77f057fd8694
Kristen Ingelman <kristeningelman@hotmail.com>
2020-12-09 17:49:21 -0800

    created Book Model

--- week11/Models/Book.js
+++ week11/Models/Book.js
@@ -6,6 +6,10 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   // put properties you would like each book to have
   title: {type: String, required: true, maxLength:100},
-  author: {type: String, required: true, default: "anonymous"}
+  author: {type: String, required: true, default: "anonymous"},
+  chapters:[chapterSchema]
 
 });
+
+// compile into a Model
+exports.Book = mongoose.model('Book', bookSchema);
